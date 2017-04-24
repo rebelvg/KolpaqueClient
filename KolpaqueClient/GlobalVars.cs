212c31e26fb0f95b0014db6a131108cdff3c56a6
Rebel <johnhartigan@ya.ru>
2017-04-24 15:01:07 +0300

    no message

--- KolpaqueClient/GlobalVars.cs
+++ KolpaqueClient/GlobalVars.cs
@@ -17,7 +17,7 @@ namespace KolpaqueClient
 {
     public partial class Form1 : Form
     {
-        string clientVersion = "0.292";
+        string clientVersion = "0.3.0";
 
         string twitchApiAppKey = "dk330061dv4t81s21utnhhdona0a91x";
 
